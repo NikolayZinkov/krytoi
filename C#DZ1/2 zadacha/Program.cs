31f0094b37f5ed103b6c898ce29e086b2c8e6357
Nikolay <nikolay_zinkov@mail.ru>
2022-11-12 17:00:21 +0300

    Наибольшее из 3

---
+++ C#DZ1/2 zadacha/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.Write ("Введите 1 число:");
+int num = Convert.ToInt32(Console.ReadLine ());
+Console.Write ("Введите 2 число:");
+int num1 = Convert.ToInt32(Console.ReadLine ());
+Console.Write ("Введите 3 число:");
+int num2 = Convert.ToInt32(Console.ReadLine ());
+
+if (num > num1&num > num2 ) {
+    Console.WriteLine ( $"Наибольшее число {num}");
+}
+else if (num1 > num&num1 > num2 ) {
+    Console.WriteLine ( $"Наибольшее число {num1}");
+}
+else if (num2 > num1&num2 > num ) {
+    Console.WriteLine ( $"Наибольшее число {num2}");
+}
