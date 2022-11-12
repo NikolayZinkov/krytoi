800343e89160f1fe04c58d2d62702a7427b79732
Nikolay <nikolay_zinkov@mail.ru>
2022-11-12 16:15:07 +0300

    Наибольшее, Наименьшее

---
+++ C#DZ1/1 zadacha/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+Console.Write ("Введите 1 число:");
+int num = Convert.ToInt32(Console.ReadLine ());
+Console.Write ("Введите 2 число:");
+int num1 = Convert.ToInt32(Console.ReadLine ());
+if (num > num1){
+   Console.WriteLine ( $"Наибольшее число {num}, Наименьшее число {num1}");
+}
+else {
+    Console.WriteLine ( $"Наибольшее число {num1}, Наименьшее число {num}");
+}
