a5dbe1e0a07ecc5855b864d0f233d2e5d88115f0
Nikolay <nikolay_zinkov@mail.ru>
2022-11-15 15:58:04 +0300

    2 Число в трехзначном

---
+++ C#DZ2/1 zadacha/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write ("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine ());
+ 
+ int a = num % 10;
+ int i = (num - a) % 100;
+
+int chislo = 0;
+ int k = 10;
+while (k <= i)
+ {
+k= k + 10;
+chislo++;
+ if (k > i)
+{
+break;
+}
+ }
+Console.WriteLine (chislo);
