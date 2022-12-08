a9d94586060f9e9db24693f3321fe8ba84222f25
Nikolay <nikolay_zinkov@mail.ru>
2022-12-08 11:18:07 +0300

    Максимальное минимальное в массиве

---
+++ C#DZ5/zadacha 3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+double [] array = new double[10];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    Console.Write ($"{array[i]}, ");
+}
+
+double max = 0;
+double min = 1000;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    else if (array[i] < min)
+    {
+        min = array[i];
+    }
+}
+
+Console.WriteLine($"Максимальное число массива {max}, минимальное {min}");
