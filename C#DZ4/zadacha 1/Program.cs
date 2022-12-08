8c4806e808ac157f70253414760a90d5c84d1952
Nikolay <nikolay_zinkov@mail.ru>
2022-12-08 06:45:48 +0300

    Натуральная степень

---
+++ C#DZ4/zadacha 1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine ("Введите число вводимое в степень: ");
+int number = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine ("Введите степень числа: ");
+int degree = Convert.ToInt32(Console.ReadLine ());
+int count = 1;
+int total = number;
+while (count < degree)
+{
+    total = number * total;
+    count++;
+}
+Console.WriteLine($"{number} в степени {degree} = {total}");
